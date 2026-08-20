2f69456215ce0d572cecd7c4996d5bd688395000
sinec1 <187554295+sinec1@users.noreply.github.com>
2026-08-20 09:59:38 -0700

    implement getUserChoice

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ function getComputerChoice(){
     return choice;
 }
 
-
-
+function getUserChoice(){
+    let choice = prompt("Please choose between rock, paper, or scissors! (lowercase no spaces!)");
+    switch (choice){
+        case "rock":
+            choice = 0;
+            break;
+        case "paper":
+            choice = 1;
+            break;
+        case "scissors":
+            choice = 2;
+            break;
+    }
+    return choice;
+}
