723ab74126baca59a5a69abb98b37837259fd966
sinec1 <187554295+sinec1@users.noreply.github.com>
2026-08-20 10:26:12 -0700

    implement playRound function

--- script.js
+++ script.js
@@ -25,5 +25,48 @@ function getUserChoice(){
 }
 
 function playRound(humanChoice, computerChoice){
-
+    switch (humanChoice) {
+        case "scissors":
+            switch(computerChoice){
+                case "scissors":
+                    console.log("It's a draw!")
+                    break;
+                case "paper":
+                    console.log("You win! Scissors beats paper.");
+                    humanScore++;
+                    break;
+                case "rock":
+                    console.log("You lose! Rock beats scissors.");
+                    computerScore++;
+            }
+            break;
+        case "rock":
+            switch(computerChoice){
+                case "rock":
+                    console.log("It's a draw!")
+                    break;
+                case "scissors":
+                    console.log("You win! Rock beats scissors.");
+                    humanScore++;
+                    break;
+                case "paper":
+                    console.log("You lose! Paper beats rock.");
+                    computerScore++;
+            }
+            break;
+        case "paper":
+            switch(computerChoice){
+                case "paper":
+                    console.log("It's a draw!")
+                    break;
+                case "rock":
+                    console.log("You win! Paper beats rock.");
+                    humanScore++;
+                    break;
+                case "scissors":
+                    console.log("You lose! Scissors beats paper.");
+                    computerScore++;
+            }
+            break;
+    }
 }
