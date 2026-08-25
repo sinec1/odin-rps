5e0ba6fc259ffdc897b68741d110a40414742128
sinec1 <187554295+sinec1@users.noreply.github.com>
2026-08-25 12:27:05 -0700

    fully implement score counters, add text for round result
    -remove redundant playGame function which was no longer used

--- script.js
+++ script.js
@@ -21,65 +21,54 @@ function getUserChoice(){
     return choice;
 }
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice){
         switch (humanChoice) {
             case "scissors":
                 switch(computerChoice){
                     case "scissors":
-                        console.log("It's a draw!")
+                        roundResult.textContent = "It's a draw!";
                         break;
                     case "paper":
-                        console.log("You win! Scissors beats paper.");
+                        roundResult.textContent = "You win! Scissors beats paper.";
                         humanScore++;
                         break;
                     case "rock":
-                        console.log("You lose! Rock beats scissors.");
+                        roundResult.textContent = "You lose! Rock beats scissors.";
                         computerScore++;
                 }
                 break;
             case "rock":
                 switch(computerChoice){
                     case "rock":
-                        console.log("It's a draw!")
+                        roundResult.textContent = "It's a draw!";
                         break;
                     case "scissors":
-                        console.log("You win! Rock beats scissors.");
+                        roundResult.textContent = "You win! Rock beats scissors.";
                         humanScore++;
                         break;
                     case "paper":
-                        console.log("You lose! Paper beats rock.");
+                        roundResult.textContent = "You lose! Paper beats rock.";
                         computerScore++;
                 }
                 break;
             case "paper":
                 switch(computerChoice){
                     case "paper":
-                        console.log("It's a draw!")
+                        roundResult.textContent = "It's a draw!";
                         break;
                     case "rock":
-                        console.log("You win! Paper beats rock.");
+                        roundResult.textContent = "You win! Paper beats rock.";
                         humanScore++;
                         break;
                     case "scissors":
-                        console.log("You lose! Scissors beats paper.");
+                        roundResult.textContent = "You lose! Scissors beats paper.";
                         computerScore++;
                 }
                 break;
         }
+        hScore.textContent = `Human Score: ${humanScore}`;
+        cScore.textContent = `Computer Score: ${computerScore}`;
     }
-
-    // let round = 1;
-    // while (round <=5){
-    //     let humanChoice = getUserChoice();
-    //     let computerChoice = getComputerChoice();
-    //     playRound(humanChoice, computerChoice);
-    //     round++;
-    // }
-}
 
 const container = document.getElementById("container")
 const rockButton = document.getElementById("rock");
@@ -98,9 +87,14 @@ hScore.textContent = `Human Score: ${humanScore}`;
 const cScore = document.createElement("div");
 cScore.textContent = `Computer Score: ${computerScore}`;
 
+const roundResult = document.createElement("div");
+roundResult.textContent = `No games played!`;
+
+
 scoreContainer.appendChild(hScore);
 scoreContainer.appendChild(cScore);
 container.appendChild(scoreContainer);
+container.appendChild(roundResult);
 
 
 container.addEventListener('click', (e) => {
